00d0869ea94c81cc6c8efcd174b3f83b2f027bbb
syasaf3389 <123484165+SJfigueroaV@users.noreply.github.com>
2026-04-19 20:04:10 -0500

    Refactorizar vehicle struct y nombre de las funciones

--- parqueadero/parqueadero.h
+++ parqueadero/parqueadero.h
@@ -1,27 +1,30 @@
+
 #pragma once
 #include <ctime>
 #include <string>
 #include "mapa.h"
 
-#define M_VEHICLES 50
+const int M_VEHICLES = 50;
 
 struct Vehicle {
-  int ROW;
-  int COL;
+  int         row;      // minuscula para no chocar con la constante ROW de mapa.h
+  int         col;      // minuscula para no chocar con la constante COL de mapa.h
   std::time_t checkIn;
   std::string plate;
 };
 
 struct Parking {
   Vehicle vehicles[M_VEHICLES];
-  int tVehicles;
-  int map[ROW][COL];
-  bool aval[ROW][COL];
+  int     tVehicles;
+  int     map[ROW][COL];
+  bool    aval[ROW][COL];
 };
 
-void menu(Parking* p);
-void buildParking(Parking* p);
-void chekIn(Parking* p);
-void showAval(Parking* p);
-void showVehicles(Parking* p);
-void calcPay(std::time_t checkIn, std::time_t checkOut);
+void   buildParking (Parking* p);
+void   menu         (Parking* p);
+void   checkInVeh   (Parking* p);
+void   checkOutVeh  (Parking* p);
+void   showAval     (Parking* p);
+void   showVehicles (Parking* p);
+double calcPay      (std::time_t checkIn, std::time_t checkOut);
+void   exportHTML   (Parking* p, int animRow, int animCol, bool leaving);
