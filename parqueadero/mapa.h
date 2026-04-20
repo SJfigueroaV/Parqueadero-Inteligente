8b3822d40df0ac9efbd11c849c1c290b5abe2429
syasaf3389 <123484165+SJfigueroaV@users.noreply.github.com>
2026-04-19 20:01:05 -0500

    Correccion errores de nomenclatura para evitar errores de compatibilidad

--- parqueadero/mapa.h
+++ parqueadero/mapa.h
@@ -1,13 +1,13 @@
 #pragma once
 
+#define WALL    0
 #define PARKING 1
-#define WALL 0
-#define ROAD 2
-#define ENTRY 3
-#define OUT 4
+#define ROAD    2
+#define ENTRY   3
+#define OUT     4
 
 const int ROW = 16;
 const int COL = 16;
 
-void buildMap(int mapa[ROW][COL]);
-void showMap(int mapa[ROW][COL], bool aval[ROW][COL]);
+void buildMap(int map[ROW][COL]);
+void showMap(int map[ROW][COL], bool aval[ROW][COL]);
